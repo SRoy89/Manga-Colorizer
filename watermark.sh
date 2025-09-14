8f0d3f8323de14f8ea3f90a2ca6c2a2fedab273e
SRoy <subhodeeproy89@gmail.com>
2025-09-14 12:59:33 +0000

    Added webp and watermark scripts

---
+++ watermark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BASE_DIR="/content/Manga-Colorizer"
+SCRIPT="$BASE_DIR/watermark.py"
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <append|crop> [output_folder]"
+  exit 1
+fi
+
+MODE=$1
+ROOT_DIR=${2:-"/content/Manga/Output"}
+
+# Run Python script
+python3 "$SCRIPT" "$MODE" --root "$ROOT_DIR"
